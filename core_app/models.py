0c8763322ef52315b33a73558050a7a944cd1e6d
AdemiluaAdeola <ademiluaadeola969@gmail.com>
2023-10-07 23:30:45 +0100

    models.py file debugged successfully

--- core_app/models.py
+++ core_app/models.py
@@ -1,22 +1,21 @@
 from django.db import models
 from auth_app.models import User
-from django_countries import Countries
 
 # Create your models here.
 class UserInfo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
-    phone = models.IntegerField()
+    phone = models.CharField(max_length=10)
     sex = models.CharField(max_length=10, choices=(('Male', 'Male'), ('Female', 'Female')))
     date= models.DateField(auto_now_add=True,null=True)
     pin = models.IntegerField()
 
     @property
     def get_phone_number(self):
-        return "+234" + int(self.phone)
+        return "+234" + self.phone
 
     @property
     def get_account_number(self):
-        return "+234" + int(self.phone)
+        return self.phone
 
     def __str__(self):
         return self.user.username
